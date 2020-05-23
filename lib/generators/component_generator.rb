6361a5023041130527d72ebf5678a12c57fa859e
James Patrick <james@jpatrick.io>
2020-05-23 02:39:35 -0400

    Corrected naming files .css instead of .pcss

--- lib/generators/component_generator.rb
+++ lib/generators/component_generator.rb
@@ -6,12 +6,12 @@ class ComponentGenerator < Rails::Generators::Base
   argument :component_name, required: true, desc: 'Component name, e.g: button'
 
   def create_css_file
-    create_file "#{component_path}/#{component_name}.css"
+    create_file "#{component_path}/#{component_name}.pcss"
   end
 
   def create_js_file
     create_file "#{component_path}/#{component_name}.js" do
-      "import \"./#{component_name}.css\";\n"
+      "import \"./#{component_name}.pcss\";\n"
     end
   end
 
